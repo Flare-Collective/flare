74b439e8ad4e9121c4c19582fd84d7cd53098986
CreedDE <tim-holz1@web.de>
2024-02-10 23:37:30 +0100

    bug: 🔐 fixing env vars in middlware

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -12,9 +12,7 @@ export const createClient = (request: NextRequest) => {
 
   const supabase = createServerClient(
     env.NEXT_PUBLIC_SUPABASE_URL,
-    env.NEXT_PUBLIC_ANON_KEY,
-    // process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     {
       cookies: {
         get(name: string) {

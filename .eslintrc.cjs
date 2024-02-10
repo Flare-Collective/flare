fd76d7699702749cb8054be5c2848a384ea33216
CreedDE <tim-holz1@web.de>
2024-02-10 23:46:12 +0100

    build: stop bitching eslint 🚨

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -13,6 +13,8 @@ const config = {
   rules: {
     // These opinionated rules are enabled in stylistic-type-checked above.
     // Feel free to reconfigure them to your own preference.
+    "@typescript-eslint/no-empty-interface": "warn",
+    "@typescript-eslint/no-unsafe-argument": "warn",
     "@typescript-eslint/array-type": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
 

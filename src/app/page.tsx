1880ab50af3825eefea240309253614c17e580fe
CreedDE <tim-holz1@web.de>
2024-02-10 05:01:04 +0100

    📦 add shadcn library

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { unstable_noStore as noStore } from "next/cache";
 import Link from "next/link";
-
 import { CreatePost } from "~/app/_components/create-post";
 import { api } from "~/trpc/server";
 
